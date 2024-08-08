a6dccf8c768d5131bb988f2072e164fc4b290930
Victor Schiavo Pereira <sn1073567@cfp127aluno.local>
2024-08-08 17:11:07 -0300

    Adicionado validação das palavras via API

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let angulo = 0;
 let animationFrameId;
+const btnVerificar = document.getElementById("verificar")
+const inputLetras = document.querySelector("#palavra")
+const palindromos = []
+let dadosApi;
+
 
 function animarGradiente(speed = 3) {
   angulo += speed;
-  angulo >= 359 ? (angulo = 0) : null;
   document.documentElement.style.setProperty("--angulo", `${angulo}deg`);
   animationFrameId = requestAnimationFrame(() => animarGradiente(speed));
 }
@@ -18,7 +22,43 @@ function stopAnimation() {
     : (animationFrameId = null);
 }
 
-startAnimation(3);
+startAnimation(5);
+
+const adicionarPalavra = function (palavra) {
+  const data = new Date();
+  const dia = data.getDate();
+  const mes = data.getMonth();
+  const ano = data.getFullYear();
+  const hora = data.getHours();
+  const minutos = data.getMinutes();
+
+  console.log(data)
+  const palavraObj = {
+    palindromo: palavra,
+    numLetras: palavra.length,
+    discovery: `${dia}/${mes}/${ano} - ${hora}:${minutos}`,
+  }
+  palindromos.push(palavraObj)
+}
+
+const atualizarPainel = function () {
+
+}
+
+
+const chamarApiDicionario = function (url) {
+  const requisição = `https://api.dicionario-aberto.net/word/${url}`
+  fetch(requisição)
+    .then(resposta => resposta.json())
+    .then(dados => {
+      if (!dados.length) {
+        throw new Error("Essa palavra não existe...")
+      }
+      dadosApi = dados
+    })
+    .catch(erro => alert(erro))
+    .finally(console.log("requisição finalizada"))
+}
 
 function isPalindrome(word) {
   const reverse = word.split("").reverse().join("");
@@ -30,3 +70,8 @@ function isPalindrome(word) {
     else return (allMatch = true);
   }
 }
+
+btnVerificar.addEventListener("click", () => {
+  if (isPalindrome(inputLetras.value)) btnVerificar.textContent = "✔"
+  else btnVerificar.textContent = "❌"
+})
